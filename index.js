48a9554bf5180c903dc4e6da875eb4f3578c6462
Bex Akeakamai <bex.akeakamai@gmail.com>
2021-08-26 13:01:41 -0400

    js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+/**
+Challenge: 
 
+When the button is clicked, call out to the Bored API
+(URL: https://apis.scrimba.com/bored/api/activity)
+and replace the h4 with the activity from the API
+
+*/
+
+document.querySelector("button").addEventListener("click", function () {
+  fetch("https://apis.scrimba.com/bored/api/activity")
+    .then((response) => response.json())
+    .then((data) => {
+      document.querySelector("h4").textContent = data.activity;
+    });
+});
